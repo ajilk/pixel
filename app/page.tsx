57ded810f83a71307d0dc81fcc07fd8080790827
Azimjon Ilkhomov <ajilk@meta.com>
2024-07-15 11:09:52 -0400

    Fix character

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <>
       <p className="text-gray-500 pb-5">
         The Meta Pixel is a <b>piece of code</b> on your website that can help you{' '}
         <b>better understand the effectiveness of your advertising</b> and the actions people take on your site, like
-        visiting a page or adding an item to their cart. You'll also be able to <b>see when customers took an action</b>
+        visiting a page or adding an item to their cart. You will also be able to <b>see when customers took an action</b>
         after seeing your ad on Facebook and Instagram, which can <b>help you with retargeting</b>.
       </p>
       <p className="text-gray-500">
